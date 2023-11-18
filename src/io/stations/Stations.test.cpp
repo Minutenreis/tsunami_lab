40f8551ee400d2c5c5edd0b3215b76376981a997
Justus Dreßler <minutenreis@gmail.com>
2023-11-18 17:50:22 +0100

    Test Reading the CSV

--- src/io/stations/Stations.test.cpp
+++ src/io/stations/Stations.test.cpp
@@ -6,3 +6,20 @@
  * @section DESCRIPTION
  * Stations Controller Class tests
  */
+#include <catch2/catch.hpp>
+#include "Stations.h"
+
+TEST_CASE("Test Reading the Json", "[StationsRead]")
+{
+    std::string l_path = "src/data/test.json";
+    tsunami_lab::io::Stations l_stations(l_path);
+
+    REQUIRE(l_stations.getT() == 2);
+    REQUIRE(l_stations.getStations().size() == 2);
+    REQUIRE(l_stations.getStations()[0].name == "Radio 1");
+    REQUIRE(l_stations.getStations()[0].x == 9);
+    REQUIRE(l_stations.getStations()[0].y == 0);
+    REQUIRE(l_stations.getStations()[1].name == "Radio 2");
+    REQUIRE(l_stations.getStations()[1].x == 14);
+    REQUIRE(l_stations.getStations()[1].y == 0);
+}
