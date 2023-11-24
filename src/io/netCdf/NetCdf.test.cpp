479a511ba6b7da24478c045e4545bfe7d405573e
Justus Dreßler <minutenreis@gmail.com>
2023-11-24 02:52:04 +0100

    Tests now write a netCdf

--- src/io/netCdf/NetCdf.test.cpp
+++ src/io/netCdf/NetCdf.test.cpp
@@ -5,5 +5,31 @@
  *
  * Test for the netCdf interface.
  **/
+#include <catch2/catch.hpp>
+#include "NetCdf.h"
+#include "../IoWriter.h"
+#include <filesystem>
 
-// TODO: Implement tests for the netCdf interface.
+TEST_CASE("Test Writing NetCDF Files", "[NetCdfWrite]")
+{
+    tsunami_lab::io::IoWriter *l_writer = new tsunami_lab::io::NetCdf();
+    tsunami_lab::t_real l_b[16] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+    l_writer->init(1, 2, 2, 4, 1, 1, 0, 0, l_b);
+
+    tsunami_lab::t_real l_h[16] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+    tsunami_lab::t_real l_hu[16] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+    tsunami_lab::t_real l_hv[16] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+    l_writer->write(l_h, l_hu, l_hv, 0, 0.0);
+
+    tsunami_lab::t_real l_h2[16] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17};
+    tsunami_lab::t_real l_hu2[16] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17};
+    tsunami_lab::t_real l_hv2[16] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17};
+    l_writer->write(l_h2, l_hu2, l_hv2, 1, 0.1);
+
+    delete l_writer;
+
+    // check if file exists
+    REQUIRE(std::filesystem::exists("output.nc"));
+
+    // todo: read data back in and compare
+}
