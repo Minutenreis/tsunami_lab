9ab4ba5493e4de280779ff914acbc77f540fe16f
Justus Dreßler <minutenreis@gmail.com>
2024-01-24 16:52:48 +0100

    Updated SLURM script for Ara GPU

--- tsunami_lab_reis.sh
+++ tsunami_lab_reis.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=tsunami_lab_reis
 #SBATCH --output=tsunami_lab_reis.output
 #SBATCH --error=tsunami_lab_reis.err
-#SBATCH --partition=s_hadoop,s_standard
+#SBATCH --partition=gpu_v100
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --time=11:00:00
@@ -13,6 +13,7 @@
 module load tools/python/3.8
 module load compiler/gcc/11.2.0
 module load compiler/intel/2020-Update2
+module load nvidia/cuda/11.7
 python3.8 -m pip install --user scons
 python3.8 -m pip install --user distro
 
@@ -20,10 +21,5 @@ python3.8 -m pip install --user distro
 # Execute the compiled program
 date
 cd /beegfs/gi24ken/tsunami_lab
-scons comp=g++ cxxO=-Ofast
-for i in {1..72}
-do
-    export OMP_NUM_THREADS=$i
-    export OMP_PLACES=numa_domains
-    ./build/tsunami_lab -u "Tsunami2d output/tohoku_gebco20_usgs_250m_displ.nc output/tohoku_gebco20_usgs_250m_bath.nc 18000" -i 4000
-done
+scons
+./build/tsunami_lab -t 10 -u "Tsunami2d output/tohoku_gebco20_usgs_250m_displ.nc output/tohoku_gebco20_usgs_250m_bath.nc 18000" -c 4000
