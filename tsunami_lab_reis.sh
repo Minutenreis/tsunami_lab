9b1fa836e8b7b13f88f14103d23945494af448de
Justus Dreßler <minutenreis@gmail.com>
2024-01-29 18:10:17 +0100

    shell script to test multiple resolutions

--- tsunami_lab_reis.sh
+++ tsunami_lab_reis.sh
@@ -5,14 +5,13 @@
 #SBATCH --partition=gpu_v100,gpu_p100,gpu_a100
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --time=1:10:00
+#SBATCH --time=11:00:00
 #SBATCH --cpus-per-task=48
 
 # Load any necessary modules (if needed)
 # module load mymodule
 module load tools/python/3.8
 module load compiler/gcc/11.2.0
-module load compiler/intel/2020-Update2
 module load nvidia/cuda/11.7
 python3.8 -m pip install --user scons==4.0.1
 python3.8 -m pip install --user distro
@@ -21,5 +20,8 @@ python3.8 -m pip install --user distro
 # Execute the compiled program
 date
 cd /beegfs/gi24ken/tsunami_lab
-scons
+scons cxxO=-Ofast
 ./build/tsunami_lab -t 1 -u "Tsunami2d output/tohoku_gebco20_usgs_250m_displ.nc output/tohoku_gebco20_usgs_250m_bath.nc 18000" -c 4000
+./build/tsunami_lab -t 1 -u "Tsunami2d output/tohoku_gebco20_usgs_250m_displ.nc output/tohoku_gebco20_usgs_250m_bath.nc 18000" -c 2000
+./build/tsunami_lab -t 1 -u "Tsunami2d output/tohoku_gebco20_usgs_250m_displ.nc output/tohoku_gebco20_usgs_250m_bath.nc 18000" -c 1000
+./build/tsunami_lab -t 1 -u "Tsunami2d output/tohoku_gebco20_usgs_250m_displ.nc output/tohoku_gebco20_usgs_250m_bath.nc 18000" -c 500
